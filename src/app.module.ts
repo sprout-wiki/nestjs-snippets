e66270f44f802cbde493be29403690d22b0bcfb0
Jaeho Jeon <88sprout@gmail.com>
2025-07-25 09:30:16 +0000

    typeorm, db, config .env 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Demo } from './demo/entities/demo.entity';
 import { DemoModule } from './demo/demo.module';
 
 @Module({
-  imports: [DemoModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 모든 모듈에서 사용 가능
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mariadb',
+        host: config.get('DB_HOST'),
+        port: +config.get<number>('DB_PORT'),
+        username: config.get('DB_USER'),
+        password: config.get('DB_PASS'),
+        database: config.get('DB_NAME'),
+        entities: [Demo],
+        synchronize: true,
+      }),
+    }),
+    DemoModule,
+  ],
 })
 export class AppModule {}
